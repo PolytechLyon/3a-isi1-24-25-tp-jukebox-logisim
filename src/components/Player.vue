965637101e5d53ba30845f365db072d2310a50f7
Clément <clement.reniers@etu.univ-lyon1.fr>
2025-01-07 16:58:36 +0100

    added play/pause

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -1,11 +1,22 @@
 <script setup>
-import { watchEffect } from 'vue';
+import { watchEffect, useTemplateRef } from 'vue';
 import { usePlaylist } from '../composables/usePlaylist';
-const { currentSong, getCurrentSong } = usePlaylist();
+const { getCurrentSong } = usePlaylist();
 
 watchEffect(() => {
     console.log('Current song:', getCurrentSong());
 });
+
+const togglePlayPause = () => {
+    const track = document.getElementById('track');
+    if (track.paused) {
+        track.play();
+        document.getElementById('playPauseButton').innerText = 'Pause';
+    } else {
+        track.pause();
+        document.getElementById('playPauseButton').innerText = 'Play';
+    }
+};
 </script>
 
 <template>
@@ -14,8 +25,8 @@ watchEffect(() => {
         <div>
             <div v-if="getCurrentSong()">
                 Now playing: {{ getCurrentSong() ? getCurrentSong().name : '' }}
-                <a href="#">Pause</a>
-                <audio :src="getCurrentSong() ? getCurrentSong().url : ''" controls></audio>
+                <a href="#" @click="togglePlayPause" id="playPauseButton">Play</a>
+                <audio :src="getCurrentSong() ? getCurrentSong().url : ''" id="track"></audio>
             </div>
             <div v-else>
                 Choose a track to play.
