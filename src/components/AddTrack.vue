d4a8db9873a096d6062748ef7676fa3abb12db72
Persson-dev <sim16.prib@gmail.com>
2025-01-07 15:56:53 +0100

    better add track

--- src/components/AddTrack.vue
+++ src/components/AddTrack.vue
@@ -3,11 +3,23 @@ import { ref } from 'vue';
 
 let addTrackOptions = ref(["By URL", "Via file upload"]);
 let uploadFile = ref(false);
+let songURL = ref("");
 
 function onChange(event) {
 	let option = event.target.value;
 	uploadFile.value = (option == addTrackOptions.value[1]);
+	console.log(songURL);
 }
+
+function clickURL(event) {
+	console.log("URL : " + songURL.value);
+}
+
+function clickUpload(event) {
+	URL.createObjectURL();
+	console.log("Upload !");
+}
+
 </script>
 
 <template>
@@ -17,9 +29,14 @@ function onChange(event) {
 		<select id="method-selector" @change="onChange">
 			<option v-for="option in addTrackOptions">{{ option }}</option>
 		</select>
-		<input v-if="uploadFile" type="file" />
-		<input v-else type="text" placeholder="Provide URL" />
-		<button disabled>Add uploaded file</button>
+		<div v-if="uploadFile">
+			<input type="file"/>
+			<button @click="clickUpload">Add uploaded file</button>
+		</div>
+		<div v-else>
+			<input v-model="songURL" type="text" placeholder="Provide URL" />
+			<button :disabled="!songURL" @click="clickURL">Add URL</button>
+		</div>
 	</div>
 </template>
 
