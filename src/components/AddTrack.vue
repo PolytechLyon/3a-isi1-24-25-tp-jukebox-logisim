c3bb5e6d8ddd1ea351fee562eaa355890e24da43
Persson-dev <sim16.prib@gmail.com>
2025-01-07 16:34:01 +0100

    ça marche sur ma machine

--- src/components/AddTrack.vue
+++ src/components/AddTrack.vue
@@ -1,30 +1,39 @@
 <script setup>
 import { ref } from 'vue';
 
-import { usePlaylist } from '../composables/usePlaylist';
-const { playlist, addSong } = usePlaylist();
+import {usePlaylist}  from '../composables/usePlaylist';
+const { addSong } = usePlaylist();
 
 
 let addTrackOptions = ref(["By URL", "Via file upload"]);
 let uploadFile = ref(false);
 let songURL = ref("");
+let blobURL = ref("");
+let songName = ref("");
 
 function onChange(event) {
 	let option = event.target.value;
 	uploadFile.value = (option == addTrackOptions.value[1]);
-	console.log(songURL.value);
+	console.log(songURL);
 }
 
-function clickURL() {
-	addSong(songURL.value, songURL.value);
-	console.log("URL : " + songURL.value);
-    console.log(playlist);
-    songURL.value = "";
+function clickURL(event) {
+	let songNameShort = songURL.value;
+	songNameShort = songNameShort.substring(songNameShort.lastIndexOf("\\") + 1);
+	addSong(songNameShort, songURL.value);
+	songURL.value = "";
+}
+
+function uploadSelected(event) {
+	blobURL.value = URL.createObjectURL(event.target.files[0]);
+	songName.value = event.target.value;
+	songName.value = songName.value.substring(songName.value.lastIndexOf("\\") + 1);
 }
 
 function clickUpload(event) {
-	URL.createObjectURL();
-	console.log("Upload !");
+	addSong(songName.value, blobURL.value);
+	songName.value = "";
+	blobURL.value = "";
 }
 
 </script>
@@ -37,8 +46,8 @@ function clickUpload(event) {
 			<option v-for="option in addTrackOptions">{{ option }}</option>
 		</select>
 		<div v-if="uploadFile">
-			<input type="file"/>
-			<button @click="clickUpload">Add uploaded file</button>
+			<input type="file" @change="uploadSelected"/>
+			<button :disabled="!blobURL" @click="clickUpload">Add uploaded file</button>
 		</div>
 		<div v-else>
 			<input v-model="songURL" type="text" placeholder="Provide URL" />
